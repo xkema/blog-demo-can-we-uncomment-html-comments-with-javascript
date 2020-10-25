e9540c811e279c62134d83bad8443d7737e54137
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-10-26 00:06:25 +0300

    Updated manual builds for url updates

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log(`%cdebug ::`, `color:crimson;font-weight:bold;`, window.location.pathname);
 
   // experimental active class setter
-  let selector = window.location.pathname.split(/\/|\-/g).filter(item => item.trim() !== '').map(item => `.n-${item}`).join(', ');
+  let selector = window.location.pathname.split(/\/|\-|\./g).filter(item => (item.trim() !== '' && item.trim() !== 'html')).map(item => `.n-${item}`).join(', ');
   if(selector === '') {
     selector = '.n-home'
   }
