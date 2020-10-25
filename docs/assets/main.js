58155df0d8351eba144d9682c6e892b4d1625fcc
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-10-26 00:27:25 +0300

    Updated active link elemnt selector script and menu styles

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   // print file info
-  console.log(`%cdebug ::`, `color:crimson;font-weight:bold;`, window.location.pathname);
-
+  const pathname = window.location.pathname;
+  console.log(`%cdebug ::`, `color:crimson;font-weight:bold;`, pathname);
   // experimental active class setter
-  let selector = window.location.pathname.split(/\/|\-|\./g).filter(item => (item.trim() !== '' && item.trim() !== 'html')).map(item => `.n-${item}`).join(', ');
-  if(selector === '') {
-    selector = '.n-home'
+  let selector = pathname.split(/\/|\-|\./g).filter(item => (item.trim() !== '' && item.trim() !== 'html')).map(item => `.n-${item}`).join(', ');
+  if (selector === '' || pathname === '/blog-demo-dom-nodes-are-expensive/' || pathname === '/') {
+    selector = '.n-home'    
   }
   document.querySelectorAll(selector).forEach(link => link.classList.add('active'));
 });
