a1001337516676dad54df486fc873fbb58cd22df
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-10-26 13:36:15 +0300

    Updated performance timer log labels

--- docs/assets/performance-timers.js
+++ docs/assets/performance-timers.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.group(`"header" and "footer" element "querySelector" timings`);
+  // header performance
+  console.time(`document.querySelector('header')`);
+  document.querySelector('header');
+  console.timeEnd(`document.querySelector('header')`);
+  // footer performance
+  console.time(`document.querySelector('footer')`);
+  document.querySelector('footer');
+  console.timeEnd(`document.querySelector('footer')`);
+  console.groupEnd();
   // number of comment lines
   console.log(`%cdebug ::`, `color:black;font-weight:bold;`, 'number of "comments wrapper" children:', document.querySelector('.comment-nodes.fly').childNodes.length);
-  // header performance
-  console.time('querySelector - header');
-  document.querySelector('header');
-  console.timeEnd('querySelector - header');
-  // footer performance
-  console.time('querySelector - footer');
-  document.querySelector('footer');
-  console.timeEnd('querySelector - footer');
 });
